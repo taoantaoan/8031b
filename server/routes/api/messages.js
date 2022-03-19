7ffeb0dd27b1054e984e840df7e69350f6420655
taoantaoan <taoantaoan@hotmail.com>
2022-03-18 18:12:47 -0700

    Remove change that is irrelevant to the ticket
    Addressed temesghentekes comment.

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -13,10 +13,6 @@ router.post("/", async (req, res, next) => {
 
     // if we already know conversation id, we can save time and just add it to message and return
     if (conversationId) {
-      const isAuthorized = await Conversation.isConversationParticipant(senderId, conversationId);
-      if (!isAuthorized) {
-        return res.sendStatus(403);
-      }
       const message = await Message.create({ senderId, text, conversationId });
       return res.json({ message, sender });
     }
