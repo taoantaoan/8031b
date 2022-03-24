52ab6221c475fd978f94fe4b893540221e038675
taoantaoan <taoantaoan@hotmail.com>
2022-03-24 13:48:25 -0700

    Adjust styling on unread messages
    Adjusted styling on unread messages to more closely match mock when unreadMessages count is greater than 9.

--- client/src/components/Sidebar/UnreadMessages.js
+++ client/src/components/Sidebar/UnreadMessages.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
+  circle: {
     backgroundColor: '#3F92FF',
     borderRadius: '1rem',
     color: '#FFFFFF',
@@ -10,13 +10,27 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: '700',
     lineHeight: '2em',
     padding: '0.02rem 0.5rem',
+    marginRight: '20px',
+    textAlign: 'center',
+  },
+  oval: {
+    backgroundColor: '#3F92FF',
+    borderRadius: '1rem',
+    color: '#FFFFFF',
+    fontSize: '10px',
+    fontWeight: '700',
+    lineHeight: '2em',
+    padding: '0.02rem 0.6rem',
+    marginRight: '20px',
     textAlign: 'center',
   },
 }));
 
 const UnreadMessages = ({ count }) => {
   const classes = useStyles();
-  return <div className={classes.root}>{count}</div>;
+  return (
+    <div className={count > 9 ? classes.oval : classes.circle}>{count}</div>
+  );
 };
 
 export default UnreadMessages;
