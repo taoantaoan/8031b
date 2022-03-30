bd5bca562e8a27838a0ba82bccaf11eee7b83d06
taoantaoan <taoantaoan@hotmail.com>
2022-03-29 18:34:27 -0700

    Update UnreadMessages to Use Material UI Component
    Replaced div tag with custom CSS to instead Material UI Badge component.

--- client/src/components/Sidebar/UnreadMessages.js
+++ client/src/components/Sidebar/UnreadMessages.js
@@ -1,35 +1,23 @@
 import React from 'react';
+import { Badge } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-  circle: {
+  badge: {
     backgroundColor: '#3F92FF',
-    borderRadius: '1rem',
     color: '#FFFFFF',
     fontSize: '10px',
     fontWeight: '700',
     lineHeight: '2em',
-    padding: '0.02rem 0.5rem',
-    marginRight: '20px',
     textAlign: 'center',
-  },
-  oval: {
-    backgroundColor: '#3F92FF',
-    borderRadius: '1rem',
-    color: '#FFFFFF',
-    fontSize: '10px',
-    fontWeight: '700',
-    lineHeight: '2em',
-    padding: '0.02rem 0.6rem',
-    marginRight: '20px',
-    textAlign: 'center',
+    marginRight: '25px',
   },
 }));
 
 const UnreadMessages = ({ count }) => {
   const classes = useStyles();
   return (
-    <div className={count > 9 ? classes.oval : classes.circle}>{count}</div>
+    <Badge badgeContent={count} classes={{ badge: classes.badge }}></Badge>
   );
 };
 
